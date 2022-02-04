cfd05abdb051b5cff6b98087ca63ea53ba80a06f
grohde1015 <gabrielle.rohde@marquette.edu>
2022-02-04 11:26:47 -0600

    Included logic for standard dev and avg functions based on online example code, have not tested yet

--- Lab1/RohdeG_stats.cpp
+++ Lab1/RohdeG_stats.cpp
@@ -4,7 +4,32 @@
 
 // 
 
-float meanValue(std::vector<float>* data, int size); 
-float standardDev(std::vector<float>* data, int size, float average);
-float minVal(std::vector<float>* data, int size);
-float maxValue(std::vector<float>* data, int size);
+float GabeTheBabe::StatsBro::meanValue(std::vector<float>* data, int size){
+
+    float sum = 0, average = 0; 
+    int i =0;
+
+    for(i=0; i<size; i++){
+        sum = sum + (*data)[i]; 
+    }
+    average = sum / size; 
+    return average;
+
+}
+float GabeTheBabe::StatsBro::standardDev(std::vector<float>* data, int size, float average){
+
+    float stddev = 0, sum = 0, calculations = 0;
+    int i = 0; 
+
+    for(i=0; i < size; i++){
+        calculations = pow(((*data[i]) - average),2);
+        sum = sum + calculations;
+    }
+    float new_avg = new_avg / size; 
+
+    stddev = sqrt(new_avg);
+    return stddev;
+
+}
+float GabeTheBabe::StatsBro::minVal(std::vector<float>* data, int size);
+float GabeTheBabe::StatsBro::maxValue(std::vector<float>* data, int size);
