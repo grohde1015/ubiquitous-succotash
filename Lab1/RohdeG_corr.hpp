97d32ad116c6095d90447f8a8b29d6302b0300c4
grohde1015 <gabrielle.rohde@marquette.edu>
2022-02-07 13:39:11 -0600

    Created namespace and class data for corr header, modified so it was a C++ file

---
+++ Lab1/RohdeG_corr.hpp
@@ -0,0 +1,13 @@
+
+
+
+#include <math.h>
+#include <iostream> 
+
+namespace GabeTheBabe{
+    class CorrelationHehe{
+        public:
+
+            float correlationCalc(std::vector <float> *data1, std::vector <float> *data2); 
+    };
+}
