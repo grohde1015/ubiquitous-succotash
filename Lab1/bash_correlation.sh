95a9fc9ac5db297c9e7b47174e4e72fd7216d251
grohde1015 <gabrielle.rohde@marquette.edu>
2022-02-10 17:05:43 -0600

    Added correlation values to csv output file

---
+++ Lab1/bash_correlation.sh
@@ -0,0 +1,44 @@
+#! bin/sh
+
+# Gabbie Rohde
+# Date: February 9, 2022
+# Calculating correlation coefficient between epochs of data 
+
+echo "Enter concentration (either 0 or 5): "
+read concentration
+echo "Concentration is ${concentration}"
+
+if [ -f conc_corr_0${concentration}.csv ]
+then 
+    rm conc_corr_0${concentration}.csv
+fi
+
+g++ -o corr_test RohdeG_corr_test.cpp
+
+if [ ${concentration} == 0 ]  
+then 
+    touch conc_corr_0${concentration}.csv
+    for i in {1..59}
+    do
+        echo -n "${i} vs $((i+1))," >> conc_corr_0${concentration}.csv
+        ./corr_test "./EEG_DATA/ERP0${concentration}${i}.txt" "./EEG_DATA/ERP0${concentration}$((i+1)).txt"  
+    
+done
+
+elif [ ${concentration} == 5 ]
+then 
+    touch conc_corr_0${concentration}.csv
+    for i in {1..59}
+    do
+        echo -n "${i} vs $((i+1))," >> conc_corr_0${concentration}.csv
+        
+        ./corr_test "./EEG_DATA/ERP0${concentration}.${i}.txt" "./EEG_DATA/ERP0${concentration}.$((i+1)).txt" >> conc_corr_0${concentration}.csv
+
+done
+
+else
+    echo "Invalid argument. Enter concentration as 0 or 5"
+    exit 1
+fi 
+
+
