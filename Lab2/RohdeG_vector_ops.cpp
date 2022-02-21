70f9948972eb80126e1ae4f580425e7c1a715512
grohde1015 <gabrielle.rohde@marquette.edu>
2022-02-21 14:02:20 -0600

    Omg I had a bug-replaced counter i with j so sad

--- Lab2/RohdeG_vector_ops.cpp
+++ Lab2/RohdeG_vector_ops.cpp
@@ -13,33 +13,33 @@ float vectorstuff::vectors::sum(std::vector<float> *data, int size){
     return data_sum;
 }
 
-std:vector<float> vectorstuff::vectors::subtract_arrays(std::vector<float> *array1, int size1, std::vector<float> *array2){
+std::vector<float> vectorstuff::vectors::subtract_arrays(std::vector<float> *array1, int size1, std::vector<float> *array2){
 
     int i=0, j=0;
     std::vector<float> subtracted_array;
 
     // filling subtracted away with size of vector from input
     for(i=0;i<size1;i++){
-        subtract_arrays.push_back(0);
+        subtracted_arrays.push_back(0);
     }
 
     for(j=0;j<size1;j++){
-        subtract_arrays[j] = ((*array1)[j] - (*array2)[j]);
+        subtracted_arrays[j] = ((*array1)[j] - (*array2)[j]);
     }
 
-    return subtract_arrays; 
+    return subtracted_arrays; 
 }
-std::vector<float> vectorstuff::vectors::divide(std::vector<float> *data, int size, int divider){
+std::vector<float> vectorstuff::vectors::divide(std::vector<float> *data, int size, float divider){
 
     int i=0, j=0;
     std::vector<float> divide_arrays;
-
+    printf("Test");
     // filling subtracted away with size of vector from input
     for(i=0; i<size; i++){
         divide_arrays.push_back(0);
     }
 
-    for(j=0;j<size;i++){
+    for(j=0;j<size;j++){
         divide_arrays[j] = ((*data)[j] / divider);  
     }
 
