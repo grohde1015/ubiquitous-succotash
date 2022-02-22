8f167f12a41963a3717a89b455aa7f3347f9ed2a
grohde1015 <gabrielle.rohde@marquette.edu>
2022-02-22 12:59:55 -0600

    makes sure file is found and num args are right

--- Lab2/RohdeG_k_means.cpp
+++ Lab2/RohdeG_k_means.cpp
@@ -1,4 +1,5 @@
 #include "RohdeG_k_means.hpp"
+#include "RohdeG_stats.hpp"
 
 // clusterfrick::Cluster::
 
@@ -18,12 +19,54 @@ void setData(std::vector<float> data){
 }
 
 // getters
-std::string getName();
-float getMean();
-std::vector<float>* getData(); 
+std::string getName(){
+    return this->name;
+}
+float getMean(){
+    return this->mean; 
+}
+std::vector<float>* getData(){
+    return this->clusterPoint;
+}
 
-private:
-std::string name; 
-float mean;
-std::vector<float> clusterPoint; 
+// only needs one arg and it's name of datafile (log_ratio_input.dat)
+int main(int argc, char* argv[]){
 
+    clusterfrick::Cluster clusterstuff; 
+    GabeTheBabe::StatsBro stats; 
+
+    // make sure there are the correct amount of args
+    if(argc>3){
+        printf("Too many arguments\n");
+        return 11; 
+    }
+
+
+    // https://stackoverflow.com/questions/46719183/c-using-ifstream-to-read-file
+    // checking if file exists and reading in data from log ratio
+
+    std::vector<float> logVector; 
+
+    std::string fileName = argv[1]; 
+    std::ifstream rfile;
+    rfile.open(fileName); 
+    std::string fileData;
+    int count = 0;  
+
+    if(rfile.is_open()){
+        
+        while(std::getline(rfile, fileData)){
+            logVector.push_back(stof(fileData));
+            count++; 
+        }
+    }
+    else{
+        printf("File cannot be found\n");
+        return 10; 
+    }
+
+
+
+
+
+}
