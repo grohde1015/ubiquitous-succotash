9e7be32afacd0ac135c5415a4e874b030f8ae9bb
grohde1015 <gabrielle.rohde@marquette.edu>
2022-03-01 14:06:36 -0600

    Initial commit of calibration sh file

---
+++ Lab2/RohdeG_calibration.sh
@@ -0,0 +1,6 @@
+# prompts user for number of microarray points to be read and processed
+# calibrates data from each of 7 time points
+# script copies log ratio out to log_ratio_input.dat
+# cluster output files to files with clear name
+# summary file of cluster means and total numbers of sup, stat, and exp genes
+
