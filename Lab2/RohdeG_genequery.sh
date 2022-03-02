5084be5d48b715487ee2502a6fefbd440e10fafe
grohde1015 <gabrielle.rohde@marquette.edu>
2022-03-01 21:59:14 -0600

    gene query bash file

--- Lab2/RohdeG_genequery.sh
+++ Lab2/RohdeG_genequery.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+
 # gene query
+
+echo "Gene sequence: "
+read geneSequence
+
+for i in {0..6}
+do
+    if grep -Fq "$geneSequence" supressed_genes_${i}.txt
+    then 
+        echo "Time ${i} gene is Supressed"
+    elif grep -Fq "$geneSequence" stationary_genes_${i}.txt
+    then
+        echo "Time ${i} gene is Stationary"
+    elif grep -Fq "$geneSequence" expressed_genes_${i}.txt
+    then
+        echo "Time ${i} gene is Expressed"
+    else
+        echo "uh oh"
+    fi
+done
