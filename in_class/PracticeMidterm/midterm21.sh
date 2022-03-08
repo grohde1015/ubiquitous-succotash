7f4e345db5b89339db027df9177211097d4c73c6
grohde1015 <gabrielle.rohde@marquette.edu>
2022-03-07 19:59:50 -0600

    comments for bitches

--- in_class/PracticeMidterm/midterm21.sh
+++ in_class/PracticeMidterm/midterm21.sh
@@ -1,5 +1,6 @@
 #! bin/sh
 
+# run bash "Name of file" whatever China Chile
 
 # making sure file exists before getting file size
 if [ ! -f owid-covid-data.csv ]
@@ -8,19 +9,30 @@ then
     exit 1
 fi
 
+# file size
 fileSize=$( wc -l < owid-covid-data.csv )
 # getting ref country data as array
 echo $fileSize
 
 
-rowNum=1
+rowNum=1 # this is for iterating through the whole file
+#https://www.xmodulo.com/read-column-data-text-file-bash.html#:~:text=To%20read%20column%20data%20from%20a%20file%20in%20bash%2C%20you,%2Dof%2Dfile%20is%20reached.
+# ifs reads a file and the , is the delimiter
+# -ra keeps backslashes and makes each field an array argument to iterate through
 while IFS=, read -ra fields; do 
 
-    # first argument
+    # first argument temporary location
     tempLoc[rowNum]=${fields[2]}
     if [[ ${tempLoc[rowNum]} == *"$2"* ]]
     then 
-        echo "${fields[2]},${fields[3]}"
+        # puts country argument 2 and date field 3 to reference location
+        echo "${fields[2]},${fields[3]}" >> refLocation[rowNum]
+    fi
+
+    # same as above but looking at 3rd argument instead of 2
+    if [[ ${tempLoc[rowNum]} == *"$3"* ]]
+    then 
+        echo "${fields[2]},${fields[3]}" >> compLocation[rowNum]
     fi
     ((rowNum=rowNum+1))
 done < "owid-covid-data.csv"
