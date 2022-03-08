b0fb10c2cb0d3d2463fc2adc7d2ef26842a1d7b6
grohde1015 <gabrielle.rohde@marquette.edu>
2022-03-07 19:44:42 -0600

    can print out 2nd arg plus time

--- in_class/PracticeMidterm/midterm21.sh
+++ in_class/PracticeMidterm/midterm21.sh
@@ -1,23 +1,27 @@
 #! bin/sh
 
-# arguments taken in
-echo "Enter header: "
-read header
 
-echo "Reference country name: "
-read refCountry
-
-echo "Comparison country:"
-read compCountry
+# making sure file exists before getting file size
+if [ ! -f owid-covid-data.csv ]
+then 
+    echo "file does not exist"
+    exit 1
+fi
 
 fileSize=$( wc -l < owid-covid-data.csv )
 # getting ref country data as array
 echo $fileSize
 
-for (( i=0;i<$fileSize;i++ ))
-do
-    if sed 's/${refCountry}//g' -eq 1 
-    then
-        echo "hi"
+
+rowNum=1
+while IFS=, read -ra fields; do 
+
+    # first argument
+    tempLoc[rowNum]=${fields[2]}
+    if [[ ${tempLoc[rowNum]} == *"$2"* ]]
+    then 
+        echo "${fields[2]},${fields[3]}"
     fi
-done
+    ((rowNum=rowNum+1))
+done < "owid-covid-data.csv"
+
