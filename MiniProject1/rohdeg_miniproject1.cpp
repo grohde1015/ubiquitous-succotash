261e97101b6a113c321fb0625376cd212b514ab1
grohde1015 <gabrielle.rohde@marquette.edu>
2022-04-08 17:09:33 -0500

    file input and logic for stats class

--- MiniProject1/rohdeg_miniproject1.cpp
+++ MiniProject1/rohdeg_miniproject1.cpp
@@ -3,8 +3,28 @@
 
 #include "/home/rohdeg/Git/ubiquitous-succotash/Lab1/RohdeG_stats.hpp"
 
-int main(){
+int main(int argc, char* argv[]){
+    // checking for file input from command line
+    if(argc != 2){
+        std::cout << "Incorrect number of arguments.\n"
+        return 10; 
+    }
 
+    // file name 
+    std::string fileName = argv[1]; 
+    std::ifstream fileData(fileName); 
 
+    // reading file data into variable -- need a stats class instance as next step
+    if(fileData.is_open()){
+        // class stuff
+
+        fileData.close();
+    }
+    else{
+        std::cout << "Cannot open file soory\n"
+        return 10; 
+    }
+
+    // stats classes set to variable values
     
 }
